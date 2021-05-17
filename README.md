668fe6795a11c7d3dff9f60a679eddadaadf54d8
Andreas Fink <andreas@afink.at>
2021-05-18 00:49:43 +0200

    add move command

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ $ npm install -g synology-image-sort
 $ synology-image-sort COMMAND
 running command...
 $ synology-image-sort (-v|--version|version)
-synology-image-sort/0.0.7 darwin-x64 node-v12.13.1
+synology-image-sort/0.0.8 darwin-x64 node-v12.13.1
 $ synology-image-sort --help [COMMAND]
 USAGE
   $ synology-image-sort COMMAND
@@ -31,6 +31,7 @@ USAGE
 
 <!-- commands -->
 * [`synology-image-sort help [COMMAND]`](#synology-image-sort-help-command)
+* [`synology-image-sort move`](#synology-image-sort-move)
 * [`synology-image-sort watch`](#synology-image-sort-watch)
 
 ## `synology-image-sort help [COMMAND]`
@@ -49,6 +50,33 @@ OPTIONS
 ```
 
 _See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v3.2.2/src/commands/help.ts)_
+
+## `synology-image-sort move`
+
+move files (images, videos) on synology nas
+
+```
+USAGE
+  $ synology-image-sort move
+
+OPTIONS
+  -d, --destination=destination  (required) destination directory
+  -e, --existing=existing        (required) existing directory
+  -f, --format=format            [default: YYYY/MM.YYYY/DD.MM.YYYY] date format
+  -h, --help                     show CLI help
+  -n, --name=name                [default: DD.MM.YYYY-HH.mm.ss.SSS] file name
+  -s, --source=source            (required) source directory
+  -t, --tags                     set folder tags
+  -u, --unknown=unknown          (required) unknown directory
+
+EXAMPLES
+  $ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing
+  $ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing -t
+  $ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing -f YYYY/MM.YYYY/DD.MM.YYYY
+  $ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing -n DD.MM.YYYY-HH.mm.ss.SSS
+```
+
+_See code: [src/commands/move.ts](https://github.com/afinkndreas/synology-image-sort/blob/v0.0.8/src/commands/move.ts)_
 
 ## `synology-image-sort watch`
 
@@ -75,5 +103,5 @@ EXAMPLES
   $ synology-image-sort watch -s ./source -d ./destination -u ./unknown -e ./existing -n DD.MM.YYYY-HH.mm.ss.SSS
 ```
 
-_See code: [src/commands/watch.ts](https://github.com/afinkndreas/synology-image-sort/blob/v0.0.7/src/commands/watch.ts)_
+_See code: [src/commands/watch.ts](https://github.com/afinkndreas/synology-image-sort/blob/v0.0.8/src/commands/watch.ts)_
 <!-- commandsstop -->
---
+++ src/commands/move.ts
@@ -0,0 +1,66 @@
+import {Command, flags} from '@oclif/command';
+import cli from 'cli-ux';
+import {getCheckDirectoryTasks} from '../helpers/getCheckDirectoryTasks';
+import {getExifData} from '../helpers/getExifData';
+import {getStatMDate} from '../helpers/getStatMDate';
+import {isImageFile} from '../helpers/isImageFile';
+import {isVideoFile} from '../helpers/isVideoFile';
+import {moveMediaFile} from '../helpers/moveMediaFile';
+import {moveUnkownFile} from '../helpers/moveUnkownFile';
+import * as recursive from 'recursive-readdir';
+
+export default class Move extends Command {
+  static description = 'move files (images, videos) on synology nas';
+  static examples = [
+    `$ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing`,
+    `$ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing -t`,
+    `$ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing -f YYYY/MM.YYYY/DD.MM.YYYY`,
+    `$ synology-image-sort move -s ./source -d ./destination -u ./unknown -e ./existing -n DD.MM.YYYY-HH.mm.ss.SSS`,
+  ];
+  static flags = {
+    help: flags.help({char: 'h'}),
+    source: flags.string({char: 's', description: 'source directory', required: true}),
+    destination: flags.string({char: 'd', description: 'destination directory', required: true}),
+    unknown: flags.string({char: 'u', description: 'unknown directory', required: true}),
+    existing: flags.string({char: 'e', description: 'existing directory', required: true}),
+    format: flags.string({char: 'f', description: 'date format', default: 'YYYY/MM.YYYY/DD.MM.YYYY'}),
+    name: flags.string({char: 'n', description: 'file name', default: 'DD.MM.YYYY-HH.mm.ss.SSS'}),
+    tags: flags.boolean({char: 't', description: 'set folder tags'}),
+  };
+
+  async run() {
+    const {flags} = this.parse(Move);
+    const source = flags.source;
+    const destination = flags.destination;
+    const unknown = flags.unknown;
+    const existing = flags.existing;
+    const format = flags.format;
+    const name = flags.name;
+    const tags = flags.tags;
+    const checkDirectoryTasks = getCheckDirectoryTasks({source, destination, unknown, existing});
+    await checkDirectoryTasks.run().catch(() => this.exit());
+    const files = await recursive(source);
+    for (let index = 0; index < files.length; index++) {
+      const filepath = files[index];
+      if (filepath.indexOf('SynoEAStream') >= 0 || filepath.indexOf('@eaDir') >= 0) return;
+      this.log(`process file '${filepath}'`);
+      const isImage = isImageFile(filepath);
+      const isVideo = isVideoFile(filepath);
+      if (isImage || isVideo) {
+        const exifData = await getExifData(filepath);
+        const exifDate = exifData ? new Date(exifData.exif.DateTimeOriginal) : undefined;
+        if (!exifDate || exifDate.getFullYear() === 1970 || isNaN(exifDate.getFullYear())) {
+          const statMDate = getStatMDate(filepath);
+          if (!statMDate) return;
+          moveMediaFile({filepath, date: statMDate, source, destination, existing, format, name, tags});
+        } else {
+          moveMediaFile({filepath, date: exifDate, source, destination, existing, format, name, tags});
+        }
+      } else {
+        const statMDate = getStatMDate(filepath);
+        if (!statMDate) return;
+        moveUnkownFile({filepath, date: statMDate, source, unknown, existing, format, name});
+      }
+    }
+  }
+}
