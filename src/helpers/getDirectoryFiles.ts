96741da89a2f94e7dbf4e8ef8cf50dacf9240213
Andreas Fink <andreas@afink.at>
2021-05-18 19:56:11 +0200

    version 0.0.15

--- src/helpers/getDirectoryFiles.ts
+++ src/helpers/getDirectoryFiles.ts
@@ -2,7 +2,7 @@ import * as recursive from 'recursive-readdir';
 
 export const getDirectoryFiles = async (directory: string): Promise<string[]> => {
   const files = await recursive(directory, [
-    (filepath) => !(filepath.indexOf('SynoEAStream') >= 0 || filepath.indexOf('@eaDir') >= 0),
+    (filepath) => filepath.indexOf('SynoEAStream') >= 0 || filepath.indexOf('@eaDir') >= 0,
   ]);
   return files;
 };
